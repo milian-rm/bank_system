c9d6e1c61dc0ee37456e6a6ec1e3274300e645b8
Compkev22 <kvelasquez-2022473@kinal.edu.gt>
2026-02-12 22:45:39 -0600

    feat: integración exitosa de rama BradleyOliva pt3

--- src/Payment/payment.routes.js
+++ src/Payment/payment.routes.js
@@ -1,11 +1,11 @@
 
 import { Router } from 'express';
 import { createPayment, getPayments } from './payment.controller.js';
-import { paymentValidator } from './payment.validator.js';
+import { validatePayment } from './payment.validator.js';
 
 const api = Router();
 
-api.post('/', [paymentValidator], createPayment);
+api.post('/', [validatePayment], createPayment);
 api.get('/', getPayments);
 
 export default api;
